9592008abc3ad0ad6291ec2755c4f44ed713e582
wim <wfjvdham@gmail.com>
2018-06-12 00:03:32 +0200

    change sorting based on points

--- app.R
+++ app.R
@@ -4,7 +4,6 @@ library(rvest)
 
 toernooi_plaatsen <- c("vlissingen", "zutphen", "arnhem", "utrecht", "leeuwarden", 
                        "eindhoven", "heerenveen", "groningen")
-men_first <- c(F, T, F, T, T, T, T, T)
 have_ticket <- c("Dirk Boehlé / Steven van de Velde", 
                  "Alexander Brouwer / Robert Meeuwsen",
                  "Jasper Bouter / Christiaan Varenhorst",
@@ -12,75 +11,84 @@ have_ticket <- c("Dirk Boehlé / Steven van de Velde",
                  "Sanne Keizer / Madelein Meppelink",
                  "Marleen Ramond-van Iersel / Joy Stubbe")
 
-get_top_3 <- function(n, uitslagen_list, gender) {
-  if (length(uitslagen_list[[n]]) > 0) {
-    result <- case_when(
-      men_first[n] && gender == "M" ~ uitslagen_list[[n]][1:3],
-      !men_first[n] && gender == "M" ~ uitslagen_list[[n]][13:15],
-      men_first[n] && gender != "M" ~ uitslagen_list[[n]][13:15],
-      !men_first[n] && gender != "M" ~ uitslagen_list[[n]][1:3]
-    )
-    if (n == 4 && gender != "M") {
-      result <- uitslagen_list[[n]][15:17]
-    }
-    if (n == 5 && gender != "M") {
-      result <- uitslagen_list[[n]][14:16]
-    }
-    return(result)
-  } 
-}
-
 ui <- fluidPage(
   titlePanel("Tussenstand voor de laatste wildcard voor het EK"),
   p("Hier wordt de tussenstand voor het laatste EK ticket weergegeven."),
   p("Wanneer er een nieuwe uitslag op eredivisiebeach.nl verschijnt wordt hij automatisch bijgewerkt."),
-  p("Vanwege de trage eredivisiebeach.nl kan het even duren..."),
+  p("Het laden kan het even duren..."),
   h1("Vrouwen"),
   tableOutput("table_women"),
   h1("Mannen"),
-  tableOutput("table_men")
+  tableOutput("table_men"),
+  a("Maracuya IT", href="http://maracuya-it.com/")
 )
 
 server <- function(input, output) {
   
-  uitslagen_data <- toernooi_plaatsen %>%
+  data <- toernooi_plaatsen %>%
     map(~paste0("https://www.eredivisiebeach.nl/tour-info/", .x)) %>%
-    map(read_html) %>%
+    map(read_html)
+  
+  team_names <- data %>%
     map(html_nodes, ".ranking_team_name") %>% 
     map(html_text)
+  positions <- data %>%
+    map(html_nodes, ".ranking_position") %>% 
+    map(html_text)
+  points <- data %>%
+    map(html_nodes, ".ranking_points") %>% 
+    map(html_text)
+  
+  df <- tibble(
+    team_names = t(as.matrix(team_names)) %>%
+      unlist(),
+    positions = t(as.matrix(positions)) %>%
+      unlist(),
+    points = t(as.matrix(points)) %>%
+      unlist()
+  ) %>%
+     mutate(points = as.integer(str_sub(points, 1, 3)),
+            positions = as.integer(positions),
+            gender = NA)
+  
+  current_gender <- "F"
+  df$gender[1] <- current_gender
+  for(i in 2:nrow(df)) {
+    if (df$positions[i - 1] <= df$positions[i]) {
+      df$gender[i] <- current_gender
+    } else {
+      #print(i)
+      if (!i %in% c(25, 49, 74)) {
+        current_gender <- if_else(current_gender == "F", "M", "F")
+      }
+      df$gender[i] <- current_gender
+    }
+  }
+  
+  calculate_standings <- function(gender_arg) {
+    df %>%
+      filter(gender == gender_arg,
+             !team_names %in% have_ticket) %>%
+      arrange(positions) %>%
+      group_by(team_names) %>%
+      slice(1:4) %>%
+      summarise(
+        punten = sum(points),
+        eerste = sum(positions == 1),
+        tweede = sum(positions == 2),
+        derde = sum(positions == 3),
+        aantal_deelnames = n()
+      ) %>%
+      arrange(desc(punten), eerste, tweede, derde) %>%
+      slice(1:6)
+  }
   
   output$table_men <- renderTable({
-    uitslagen_parsed <- data_frame(
-      team = 1:length(toernooi_plaatsen) %>%
-        map(get_top_3, uitslagen_data, "M") %>%
-        unlist(),
-      plaats = rep(c(1, 2, 3), length(team) / 3)
-    )
-    
-    uitslagen_parsed %>%
-      group_by(team) %>%
-      summarise(eerste_plekken = sum(plaats == 1, na.rm = TRUE),
-                tweede_plekken = sum(plaats == 2, na.rm = TRUE),
-                derde_plekken = sum(plaats == 3, na.rm = TRUE)) %>%
-      filter(!team %in% have_ticket) %>%
-      arrange(desc(eerste_plekken), desc(tweede_plekken), desc(derde_plekken))
+    calculate_standings("M")
   })
   
   output$table_women <- renderTable({
-    uitslagen_parsed <- data_frame(
-      team = 1:length(toernooi_plaatsen) %>%
-        map(get_top_3, uitslagen_data, "F") %>%
-        unlist(),
-      plaats = rep(c(1, 2, 3), length(team) / 3)
-    )
-    
-    uitslagen_parsed %>%
-      group_by(team) %>%
-      summarise(eerste_plekken = sum(plaats == 1, na.rm = TRUE),
-                tweede_plekken = sum(plaats == 2, na.rm = TRUE),
-                derde_plekken = sum(plaats == 3, na.rm = TRUE)) %>%
-      filter(!team %in% have_ticket) %>%
-      arrange(desc(eerste_plekken), desc(tweede_plekken), desc(derde_plekken))
+    calculate_standings("F")
   })
 }
 
