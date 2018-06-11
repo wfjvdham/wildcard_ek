b2eb6204d0345f8c4ede0474671878cf7e71fa36
wim <wfjvdham@gmail.com>
2018-06-11 18:45:17 +0200

    add women

--- app.R
+++ app.R
@@ -8,15 +8,25 @@ men_first <- c(F, T, F, T, T, T, T, T)
 have_ticket <- c("Dirk Boehlé / Steven van de Velde", 
                  "Alexander Brouwer / Robert Meeuwsen",
                  "Jasper Bouter / Christiaan Varenhorst",
-                 "Neilton Moises Santos Merces / Harley Marques Silva")
+                 "Neilton Moises Santos Merces / Harley Marques Silva",
+                 "Sanne Keizer / Madelein Meppelink",
+                 "Marleen Ramond-van Iersel / Joy Stubbe")
 
-get_top_3_men <- function(n, uitslagen_list) {
+get_top_3 <- function(n, uitslagen_list, gender) {
   if (length(uitslagen_list[[n]]) > 0) {
-    if (men_first[n]) {
-      return(uitslagen_list[[n]][1:3])
-    } else {
-      return(uitslagen_list[[n]][13:15])
+    result <- case_when(
+      men_first[n] && gender == "M" ~ uitslagen_list[[n]][1:3],
+      !men_first[n] && gender == "M" ~ uitslagen_list[[n]][13:15],
+      men_first[n] && gender != "M" ~ uitslagen_list[[n]][13:15],
+      !men_first[n] && gender != "M" ~ uitslagen_list[[n]][1:3]
+    )
+    if (n == 4 && gender != "M") {
+      result <- uitslagen_list[[n]][15:17]
     }
+    if (n == 5 && gender != "M") {
+      result <- uitslagen_list[[n]][14:16]
+    }
+    return(result)
   } 
 }
 
@@ -25,21 +35,41 @@ ui <- fluidPage(
   p("Hier wordt de tussenstand voor het laatste EK ticket weergegeven."),
   p("Wanneer er een nieuwe uitslag op eredivisiebeach.nl verschijnt wordt hij automatisch bijgewerkt."),
   p("Vanwege de trage eredivisiebeach.nl kan het even duren..."),
-  tableOutput("table")
+  h1("Vrouwen"),
+  tableOutput("table_women"),
+  h1("Mannen"),
+  tableOutput("table_men")
 )
 
 server <- function(input, output) {
   
-  output$table <- renderTable({
-    uitslagen_data <- toernooi_plaatsen %>%
-      map(~paste0("https://www.eredivisiebeach.nl/tour-info/", .x)) %>%
-      map(read_html) %>%
-      map(html_nodes, ".ranking_team_name") %>% 
-      map(html_text)
-    
+  uitslagen_data <- toernooi_plaatsen %>%
+    map(~paste0("https://www.eredivisiebeach.nl/tour-info/", .x)) %>%
+    map(read_html) %>%
+    map(html_nodes, ".ranking_team_name") %>% 
+    map(html_text)
+  
+  output$table_men <- renderTable({
     uitslagen_parsed <- data_frame(
       team = 1:length(toernooi_plaatsen) %>%
-        map(get_top_3_men, uitslagen_data) %>%
+        map(get_top_3, uitslagen_data, "M") %>%
+        unlist(),
+      plaats = rep(c(1, 2, 3), length(team) / 3)
+    )
+    
+    uitslagen_parsed %>%
+      group_by(team) %>%
+      summarise(eerste_plekken = sum(plaats == 1, na.rm = TRUE),
+                tweede_plekken = sum(plaats == 2, na.rm = TRUE),
+                derde_plekken = sum(plaats == 3, na.rm = TRUE)) %>%
+      filter(!team %in% have_ticket) %>%
+      arrange(desc(eerste_plekken), desc(tweede_plekken), desc(derde_plekken))
+  })
+  
+  output$table_women <- renderTable({
+    uitslagen_parsed <- data_frame(
+      team = 1:length(toernooi_plaatsen) %>%
+        map(get_top_3, uitslagen_data, "F") %>%
         unlist(),
       plaats = rep(c(1, 2, 3), length(team) / 3)
     )
