5a8ff66a94f15b3e2da85ad6cb1e299418d145d8
wim <wfjvdham@gmail.com>
2018-06-14 01:07:11 +0200

    changed permissions, added refresh button for reload

--- app.R
+++ app.R
@@ -11,24 +11,7 @@ have_ticket <- c("Dirk Boehlé / Steven van de Velde",
                  "Sanne Keizer / Madelein Meppelink",
                  "Marleen Ramond-van Iersel / Joy Stubbe")
 
-ui <- fluidPage(
-  titlePanel("Tussenstand voor de laatste wildcard voor het EK"),
-  p("Hier wordt de tussenstand voor het laatste EK ticket weergegeven. 
-    De 4 beste resultaten voor een team zijn bij elkaar opgeteld.
-    max_punten geeft aan hoeveel punten het team maximaal 
-    (met alleen maar eeste plekken in de resterende toernooien)
-    nog kan halen."),
-  p("Wanneer er een nieuwe uitslag op eredivisiebeach.nl verschijnt wordt hij automatisch bijgewerkt."),
-  p("Het laden kan het even duren..."),
-  h1("Vrouwen"),
-  tableOutput("table_women"),
-  h1("Mannen"),
-  tableOutput("table_men"),
-  a("Maracuya IT", href="http://maracuya-it.com/")
-)
-
-server <- function(input, output) {
-  
+scrape_data <- function() {
   data <- toernooi_plaatsen %>%
     map(~paste0("https://www.eredivisiebeach.nl/tour-info/", .x)) %>%
     map(read_html)
@@ -51,13 +34,18 @@ server <- function(input, output) {
     points = points %>%
       unlist()
   ) %>%
-     mutate(points = as.integer(str_sub(points, 1, 3)),
-            positions = as.integer(positions),
-            gender = NA)
+    mutate(points = as.integer(str_sub(points, 1, 3)),
+           positions = as.integer(positions),
+           gender = NA)
+  write_rds(df, "./data.rds")
+}
+
+parse_data <- function() {
+  df <- read_rds("./data.rds")
   
   current_gender <- "F"
-  df$gender[1] <- current_gender
   n_toernaments_played <- 1
+  df$gender[1] <- current_gender
   for(i in 2:nrow(df)) {
     if (df$positions[i - 1] <= df$positions[i]) {
       df$gender[i] <- current_gender
@@ -71,37 +59,68 @@ server <- function(input, output) {
     }
   }
   n_toernaments_played <- n_toernaments_played / 2
+  list(df, n_toernaments_played)
+}
+
+calculate_standings <- function(df, n_toernaments_played, gender_arg) {
+  df %>%
+    filter(gender == gender_arg,
+           !team_names %in% have_ticket) %>%
+    arrange(positions) %>%
+    group_by(team_names) %>%
+    slice(1:4) %>%
+    summarise(
+      punten = sum(points),
+      max_punten = as.integer(sum(
+        sort(append(rep(768, 8 - n_toernaments_played), points), decreasing = TRUE)[1:4]
+      )),
+      aantal_deelnames = n()
+    ) %>%
+    # summarise(
+    #   eerste = sum(positions == 1),
+    #   tweede = sum(positions == 2),
+    #   derde = sum(positions == 3)
+    # ) %>%
+    arrange(desc(punten)) %>%
+    filter(max(punten) <= max_punten) %>%
+    slice(1:6)
+}
+
+ui <- fluidPage(
+  titlePanel("Tussenstand voor de laatste wildcard voor het EK"),
+  p("Hier wordt de tussenstand voor het laatste EK ticket weergegeven. 
+    De 4 beste resultaten voor een team zijn bij elkaar opgeteld.
+    max_punten geeft aan hoeveel punten het team maximaal 
+    (met alleen maar eeste plekken in de resterende toernooien)
+    nog kan halen."),
+  p("Wanneer er een nieuwe uitslag op eredivisiebeach.nl 
+    verschijnt kan je hem bijwerken door op updaten te drukken"),
+  h1("Vrouwen"),
+  tableOutput("table_women"),
+  h1("Mannen"),
+  tableOutput("table_men"),
+  actionButton(inputId = "refresh", label = "Update", class="btn btn-primary"),
+  a("Maracuya IT", href="http://maracuya-it.com/")
+)
+
+server <- function(input, output) {
   
-  calculate_standings <- function(gender_arg) {
-    df %>%
-      filter(gender == gender_arg,
-             !team_names %in% have_ticket) %>%
-      arrange(positions) %>%
-      group_by(team_names) %>%
-      slice(1:4) %>%
-      summarise(
-        punten = sum(points),
-        max_punten = as.integer(sum(
-          sort(append(rep(768, 8 - n_toernaments_played), points), decreasing = TRUE)[1:4]
-        )),
-        aantal_deelnames = n()
-      ) %>%
-      # summarise(
-      #   eerste = sum(positions == 1),
-      #   tweede = sum(positions == 2),
-      #   derde = sum(positions == 3)
-      # ) %>%
-      arrange(desc(punten)) %>%
-      filter(max(punten) <= max_punten) %>%
-      slice(1:6)
-  }
+  rv <- reactiveValues(result_list = parse_data())
   
   output$table_men <- renderTable({
-    calculate_standings("M")
+    calculate_standings(rv$result_list[[1]], rv$result_list[[2]], "M")
   })
   
   output$table_women <- renderTable({
-    calculate_standings("F")
+    calculate_standings(rv$result_list[[1]], rv$result_list[[2]], "F")
+  })
+  
+  observeEvent(input$refresh, {
+    withProgress(message = 'Getting data...', value = 0, {
+      scrape_data()
+      incProgress(0.9, detail = "Calculating standings...")
+      rv$result_list <- parse_data()
+    })
   })
 }
 
